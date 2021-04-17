82049cc0140aca3a3076bdb045b3a4070cdfed60
deodanes <josedeodanes99@gmailcom>
2021-04-16 21:46:26 -0600

    Update

--- UniversidadBBDD/universidad.sql
+++ UniversidadBBDD/universidad.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 10-04-2021 a las 07:44:45
--- Versión del servidor: 10.4.17-MariaDB
--- Versión de PHP: 8.0.1
+-- Servidor: localhost
+-- Tiempo de generación: 16-04-2021 a las 22:44:51
+-- Versión del servidor: 10.3.27-MariaDB-0+deb10u1
+-- Versión de PHP: 7.3.27-1~deb10u1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `carreras` (
-  `id` int(30) NOT NULL,
+  `id` int(11) NOT NULL,
   `duracion` date NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `pensum` varchar(56) NOT NULL,
@@ -42,7 +42,7 @@ CREATE TABLE `carreras` (
 --
 
 CREATE TABLE `cursos` (
-  `id` int(30) NOT NULL,
+  `id` int(11) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `descripcion` text NOT NULL,
   `cupos` int(30) NOT NULL,
@@ -59,7 +59,7 @@ CREATE TABLE `cursos` (
 --
 
 CREATE TABLE `docente` (
-  `id` int(30) NOT NULL,
+  `id` int(11) NOT NULL,
   `nombres` varchar(50) NOT NULL,
   `apellidos` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
@@ -95,7 +95,7 @@ CREATE TABLE `estudiante` (
 --
 
 CREATE TABLE `facultad` (
-  `id` int(30) NOT NULL,
+  `id` int(11) NOT NULL,
   `nombre` varchar(102) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -106,9 +106,9 @@ CREATE TABLE `facultad` (
 --
 
 CREATE TABLE `matricula` (
-  `id` int(30) NOT NULL,
+  `id` int(11) NOT NULL,
   `fecha` date NOT NULL,
-  `id_estudiante` int(30) NOT NULL,
+  `id_estudiante` varchar(50) DEFAULT NULL,
   `id_curso` int(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -119,7 +119,7 @@ CREATE TABLE `matricula` (
 --
 
 CREATE TABLE `periodo` (
-  `id` int(30) NOT NULL,
+  `id` int(11) NOT NULL,
   `fecha_inicio` date NOT NULL,
   `fecha_finalizacion` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -131,7 +131,7 @@ CREATE TABLE `periodo` (
 --
 
 CREATE TABLE `rol` (
-  `id` int(30) NOT NULL,
+  `id` int(11) NOT NULL,
   `nombre` varchar(50) CHARACTER SET utf8 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -143,25 +143,32 @@ CREATE TABLE `rol` (
 -- Indices de la tabla `carreras`
 --
 ALTER TABLE `carreras`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_carreras_facultad` (`id_facultad`);
 
 --
 -- Indices de la tabla `cursos`
 --
 ALTER TABLE `cursos`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_cursos_periodo` (`id_periodo`),
+  ADD KEY `fk_cursos_carreras` (`id_carrera`),
+  ADD KEY `fk_cursos_docente` (`id_docente`);
 
 --
 -- Indices de la tabla `docente`
 --
 ALTER TABLE `docente`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_docente_rol` (`id_rol`);
 
 --
 -- Indices de la tabla `estudiante`
 --
 ALTER TABLE `estudiante`
-  ADD PRIMARY KEY (`carnet`);
+  ADD PRIMARY KEY (`carnet`),
+  ADD KEY `fk_estudiante_carrera` (`id_carrera`),
+  ADD KEY `fk_estudiante_rol` (`id_rol`);
 
 --
 -- Indices de la tabla `facultad`
@@ -173,7 +180,9 @@ ALTER TABLE `facultad`
 -- Indices de la tabla `matricula`
 --
 ALTER TABLE `matricula`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_matricula_estudiante` (`id_estudiante`),
+  ADD KEY `fk_matricula_curso` (`id_curso`);
 
 --
 -- Indices de la tabla `periodo`
@@ -188,14 +197,88 @@ ALTER TABLE `rol`
   ADD PRIMARY KEY (`id`);
 
 --
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `carreras`
+--
+ALTER TABLE `carreras`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `cursos`
+--
+ALTER TABLE `cursos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `docente`
+--
+ALTER TABLE `docente`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `facultad`
+--
+ALTER TABLE `facultad`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `matricula`
+--
+ALTER TABLE `matricula`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `periodo`
+--
+ALTER TABLE `periodo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `rol`
+--
+ALTER TABLE `rol`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `carreras`
+--
+ALTER TABLE `carreras`
+  ADD CONSTRAINT `fk_carreras_facultad` FOREIGN KEY (`id_facultad`) REFERENCES `facultad` (`id`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `cursos`
+--
+ALTER TABLE `cursos`
+  ADD CONSTRAINT `fk_cursos_carreras` FOREIGN KEY (`id_carrera`) REFERENCES `carreras` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_cursos_docente` FOREIGN KEY (`id_docente`) REFERENCES `docente` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_cursos_periodo` FOREIGN KEY (`id_periodo`) REFERENCES `periodo` (`id`) ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `docente`
 --
 ALTER TABLE `docente`
-  ADD CONSTRAINT `docente_ibfk_1` FOREIGN KEY (`id`) REFERENCES `rol` (`id`);
+  ADD CONSTRAINT `fk_docente_rol` FOREIGN KEY (`id_rol`) REFERENCES `rol` (`id`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `estudiante`
+--
+ALTER TABLE `estudiante`
+  ADD CONSTRAINT `fk_estudiante_carrera` FOREIGN KEY (`id_carrera`) REFERENCES `carreras` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_estudiante_rol` FOREIGN KEY (`id_rol`) REFERENCES `rol` (`id`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `matricula`
+--
+ALTER TABLE `matricula`
+  ADD CONSTRAINT `fk_matricula_curso` FOREIGN KEY (`id_curso`) REFERENCES `cursos` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_matricula_estudiante` FOREIGN KEY (`id_estudiante`) REFERENCES `estudiante` (`carnet`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
---
+++ conexion/conexion.php
@@ -0,0 +1,16 @@
+<?php
+
+session_start();
+
+$conexion = mysqli_connect('localhost', 'root','','universidad');
+
+/*
+	if (isset($conn)){
+		echo "conexion existosa";
+
+	}
+	else{
+		echo "error";
+	}*/
+
+?>
